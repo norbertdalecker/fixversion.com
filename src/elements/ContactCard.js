5383beea7da011ef3970bdd3447b3da824ff6ad2
norbertdalecker <norbertdalecker@gmail.com>
2018-04-19 11:26:02 +0200

    update contact page

--- src/elements/ContactCard.js
+++ src/elements/ContactCard.js
@@ -3,7 +3,7 @@ import logoplain from "../assets/images/logo-plain.svg";
 import Icon from 'react-icon-base'
 const ContactCard = props => (
     <img 
-    style={{display: "block", margin: "auto"}}
+    style={{display: "block", margin: "auto", paddingBottom: "60px"}}
     src={logoplain}
     width="80%" />
 )
